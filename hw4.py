aa4c893f40e912ff9d0614188043b3f95df989c8
EU\terepdmi <dmitrijs.terepa@tietoevry.com>
2021-10-01 17:22:13 +0300

    hw4

---
+++ hw4.py
@@ -0,0 +1,40 @@
+# 1. FizzBuzz
+
+for n in range(1, 100):
+
+    if n % 35 == 0:
+        print("FizzBuzz")
+        continue
+
+    elif n% 5 == 0:
+        print("Fizz")
+        continue
+
+    elif n% 7 == 0:
+        print("Buzz")
+        continue
+
+    print(n)
+
+ # 2. Christmas tree
+
+a=3
+for i in range(3):
+  print(" "*a,end="")
+  print("*"*(2*i+1))
+  a-=1
+
+
+# 3. Primes Check if entered positive number is a prime number.
+
+num = int(input("Please enter a number: "))
+if num > 1:
+    for i in range(2, int(num / 2) + 1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+else:
+    print(num, "is not a prime number")
