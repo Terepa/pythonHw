b098929556b0b3fbce2c6afe28638709352e2d79
EU\terepdmi <dmitrijs.terepa@tietoevry.com>
2021-11-02 09:51:59 +0200

    commit

--- HW13.py
+++ HW13.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 response = requests.get("https://api.openbrewerydb.org/breweries")
@@ -6,6 +8,5 @@ data = response.json()
 for x in data:
     print(x['name'], x['brewery_type'])
 
-    food = "kartupelis"
-    d = {c: c.upper() * 2 for c in food}
-    print(d['c'])
+with open("brewery.json", mode = "w") as write_file:
+    json.dump(data, write_file, indent=4)
